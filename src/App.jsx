6a8cfe2e58aaa4e0c8d9270a1f3d85ddc02c38fe
Jose <josecarlosjc30@gmail.com>
2021-11-05 19:42:05 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import { Header } from "./components/Header";
+import Header from "./components/Header";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route
+} from "react-router-dom";
+import Services from "./pages/Services";
+import Home from "./pages/Home";
 
-const App = (props) => {
+export default class App extends React.Component {
 
-   return (
-      <div className="App">
-         <Header />
-         <main className="container mx-auto py-4">
-            <p>
-               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, tempore? Laboriosam, totam facilis perspiciatis ex est quisquam, praesentium, eos nisi aut esse tempore obcaecati fugit consequatur voluptatem? Ab, temporibus quas?</p>
-         </main>
-      </div>
-   );
+   constructor(props) {
+      super(props);
+      this.state = {
+         show: true
+      }
+   }
+
+   componentDidMount() {
+      this.setState({show: true})
+   }
+
+   componentWillUnmount() {
+      this.setState({show: false})
+   }
+
+   render() {
+      return (
+         <div className="App" onWheel={(e) => { console.log(e) }}>
+            <Router>
+               <Header />
+               <main className={`container mx-auto p-4 ${(this.state.show) ? 'showOn' : 'showOff'}`}>
+               {/* <main className="container mx-auto p-4"> */}
+                  <Switch>
+                     <Route path="/services">
+                        <Services />
+                     </Route>
+                     <Route path="/blog">
+                        {/* <Blog /> */}
+                     </Route>
+                     <Route path="/about-us">
+                        {/* <About /> */}
+                     </Route>
+                     <Route path="/">
+                        <Home />
+                     </Route>
+                  </Switch>
+               </main>
+            </Router>
+         </div>
+      );
+   }
 }
-
-export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export class Header extends React.Component {
+export default class Header extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
          open: false
       }
    }
-   toggleMenu = () => {
-      this.setState({
-         open: !this.state.open
-      });
-   }
 
    render() {
       return (
-         <header className="bg-white shadow-sm sticky top-0">
-            <div className="py-4 px-5 flex justify-between sm:justify-start items-center relative">
-               <h1 className="font-bold mr-6 text-xl">Beta</h1>
+         <header className="sticky top-0 bg-gray-100">
+            <div className="py-4 px-4 flex justify-between sm:justify-start items-center relative">
+               <h1 className="font-bold mr-6 text-xl">
+                  <Link to="/">Beta</Link>
+               </h1>
                <nav className={`${(this.state.open) ? 'block' : 'hidden sm:block'} nav`}>
                   <ul className="space-y-3 sm:space-y-0">
-                     <li className="nav-item">Services</li>
-                     <li className="nav-item">Blog</li>
-                     <li className="nav-item">About us</li>
+                     <li className="nav-item">
+                        <Link to="/services">Services</Link>
+                     </li>
+                     <li className="nav-item">
+                        <Link to="/blog">Blog</Link>
+                     </li>
+                     <li className="nav-item">
+                        <Link to="/about-us">About us</Link>
+                     </li>
                   </ul>
                </nav>
-               <span className="material-icons inline-block sm:hidden cursor-pointer" onClick={this.toggleMenu}>
+               <span className="material-icons inline-block sm:hidden cursor-pointer" onClick={() => {this.setState({open: !this.state.open})}}>
                   {(this.state.open) ? 'close' : 'menu'}
                </span>
             </div>
